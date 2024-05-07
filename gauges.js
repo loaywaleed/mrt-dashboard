88f40f6cf1139e0d539dda6c3c191e4fd92fcd57
loaywaleed <loaywaleed9@gmail.com>
2024-05-07 08:29:22 +0300

    gauges

--- gauges.js
+++ gauges.js
@@ -1,13 +1,19 @@
-var dom = document.getElementById('container');
-var myChart = echarts.init(dom, 'dark', {
+var dom1 = document.getElementById('container-1');
+var myChart1 = echarts.init(dom1, 'dark', {
+  renderer: 'canvas',
+  useDirtyRect: false
+});
+var dom2 = document.getElementById('container-2');
+var myChart2 = echarts.init(dom2, 'dark', {
   renderer: 'canvas',
   useDirtyRect: false
 });
 var app = {};
 
-var option;
+var option1;
+var option2;
 
-option = {
+option1 = {
 series: [
 {
   type: 'gauge',
@@ -60,10 +66,70 @@ series: [
 ]
 };
 
-if (option && typeof option === 'object') {
-  myChart.setOption(option);
+option2 = {
+    series: [
+    {
+      type: 'gauge',
+      progress: {
+        show: true,
+        width: 18
+      },
+      axisLine: {
+        lineStyle: {
+          width: 18
+        }
+      },
+      axisTick: {
+        show: false
+      },
+      splitLine: {
+        length: 15,
+        lineStyle: {
+          width: 2,
+          color: '#999'
+        }
+      },
+      axisLabel: {
+        distance: 25,
+        color: '#999',
+        fontSize: 20
+      },
+      anchor: {
+        show: true,
+        showAbove: true,
+        size: 25,
+        itemStyle: {
+          borderWidth: 10
+        }
+      },
+      title: {
+        show: false
+      },
+      detail: {
+        valueAnimation: true,
+        fontSize: 80,
+        offsetCenter: [0, '70%']
+      },
+      data: [
+        {
+          value: 70
+        }
+      ]
+    }
+    ]
+    };
+
+if (option1 && typeof option1 === 'object') {
+  myChart1.setOption(option1);
 }
 
-window.addEventListener('resize', myChart.resize);
+if (option2 && typeof option2 === 'object') {
+    myChart2.setOption(option2);
+}
+
+  window.addEventListener('resize', function () {
+    myChart1.resize();
+    myChart2.resize();
+});
 
 
