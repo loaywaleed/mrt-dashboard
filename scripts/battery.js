13fa6fd7f3a6690a48352fa03db2410fff992999
loaywaleed <loaywaleed9@gmail.com>
2024-05-11 19:49:04 +0300

    fixing battery indicator

--- scripts/battery.js
+++ scripts/battery.js
@@ -4,11 +4,12 @@ const indicator = document.querySelector('.battery');
 const monitor = (battery) => {
   const percent = Math.floor(battery);
   const color = `hsl(${percent}, 100%, 40%)`;
+  console.log(color)
   root.style.setProperty('--color', color);
   root.style.setProperty('--width', `${percent}px`);
   indicator.innerHTML = `${percent}%`; 
 }
 
-const batteryLevel = 70
+const batteryLevel = 40
 
 monitor(batteryLevel);
