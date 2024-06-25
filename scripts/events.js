1256490843b45d564403c6cf3aaf6fc60d49f8d9
loaywaleed <loaywaleed9@gmail.com>
2024-06-25 12:13:03 +0300

    handling blink on animation sync

--- scripts/events.js
+++ scripts/events.js
@@ -1,13 +1,46 @@
-import socket from './socket.js'
-socket.on('message', () => {
-    console.log('Received message from server');
+import socket from "./socket.js";
+socket.on("message", () => {
+  console.log("Received message from server");
 });
 
-socket.on('vi_range', (data) => {
-    console.log('current, voltage, range data received', data);
-    console.log(data);
-    document.getElementById("voltage").innerText = data.voltage + ' V';
-    document.getElementById("current").innerText = data.current + ' A';
-    document.getElementById("range").innerText = data.range + ' ';
-})
+/* Blinking Signals Constants */
+const OFF = 0;
+const RIGHT = 1;
+const LEFT = 2;
+const ON = 3;
 
+/* Voltage, Current, Cruising Range */
+socket.on("vi_range", (data) => {
+  console.log("current, voltage, range data received", data);
+  document.getElementById("voltage").innerText = data.voltage + " V";
+  document.getElementById("current").innerText = data.current + " A";
+  document.getElementById("range").innerText = data.range + " ";
+});
+
+socket.on("blinkers", (data) => {
+  const blinker_signal = data.blinkers;
+  console.log("blinker data received", data);
+  let arrowLeft = document.getElementById("arrow-left");
+  let arrowRight = document.getElementById("arrow-right");
+  switch (blinker_signal) {
+    case OFF:
+      arrowLeft.classList.add("stop-blinking");
+      arrowRight.classList.add("stop-blinking");
+      break;
+    case RIGHT:
+      arrowLeft.classList.add("stop-blinking");
+      arrowRight.classList.remove("stop-blinking");
+      break;
+    case LEFT:
+      arrowLeft.classList.remove("stop-blinking");
+      arrowRight.classList.add("stop-blinking");
+      break;
+    case ON:
+      arrowLeft.classList.remove("stop-blinking");
+      arrowRight.classList.remove("stop-blinking");
+      break;
+    default:
+      console.log("invalid signal");
+      throw new Error("Invalid blinker signal");
+  }
+});
